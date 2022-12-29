6b355d64294303c157b40006f5f29c6478fd4293
Abiodun Azeez <iamhabbeboy@gmail.com>
2022-12-29 21:00:31 +0100

    Add selection animation to pm

--- extension/webviews/components/Image.svelte
+++ extension/webviews/components/Image.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let src: string;
+    export let selected: String;
 </script>
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 <img
@@ -7,5 +8,14 @@
   width="20"
   height="20"
   alt=""
+  class:selected={selected === "selected"}
   on:click
 />
+
+<style>
+      .selected {
+		background-color: #ff3e00;
+		color: white;
+    opacity: 0.2;
+	}
+</style>
--- extension/webviews/components/Sidebar.svelte
+++ extension/webviews/components/Sidebar.svelte
@@ -27,6 +27,7 @@
 
   let query: string = "";
   let vendor: string = "";
+  let selected: string = "";
 
   const search = async () => {
     if (query === "") {
@@ -43,17 +44,19 @@
 
   const handleSelection = (publisher: string) => {
     vendor = publisher;
+    selected = "selected";
   };
 
 </script>
 
 <h3>Search Package</h3>
 <input type="text" bind:value={query} />
-<Image src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669412388/go_qo9jg9.png"} on:click={() => handleSelection("goget")}/>
+
+<Image selected={selected} src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669412388/go_qo9jg9.png"} on:click={() => handleSelection("goget")}/>
 &nbsp;
-<Image src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669412388/php_tczkal.png"} on:click={() => handleSelection("composer")}/>
+<Image selected={selected} src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669412388/php_tczkal.png"} on:click={() => handleSelection("composer")}/>
 &nbsp;
-<Image src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669411991/js_zgy2wh.png"} on:click={() => handleSelection("npm")}/>
+<Image selected={selected} src={"https://res.cloudinary.com/denj7z5ec/image/upload/v1669411991/js_zgy2wh.png"} on:click={() => handleSelection("npm")}/>
 <button on:click={search}>Search</button>
 
 {#if pkgs.result.length == 0}
